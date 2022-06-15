1a93c2de843574138c6eeba9c4da563810866f65
trsdewi <76573977+trsdewi@users.noreply.github.com>
2022-06-15 11:07:21 +0800

    Update masuk.php

--- finalproject/masuk.php
+++ finalproject/masuk.php
@@ -30,38 +30,60 @@ if (isset($_POST["submit"])) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Barang Masuk</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
-<body>
+<body class="form">
+	<div class="wrap">
+		<div class="container2">
 	<h1>Input Barang Masuk</h1>
 	<form action="" method="post">
-		<ul>
-			<li>
-				<label for="id_transaksi">ID Transaksi	: <input type="text" name="id_transaksi" id="id_transaksi"></label>
-			</li>
-			<li>
-				<label for="tanggal">Tanggal	: <input type="text" name="tanggal" id="tanggal"></label>
-			</li>
-			<li>
-				<label for="kode_barang">Kode Barang	: <input type="text" name="kode_barang" id="kode_barang"></label>
-			</li>
-			<li>
-				<label for="nama_barang">Nama Barang	: <input type="text" name="nama_barang" id="nama_barang"></label>
-			</li>
-			<li>
-				<label for="supplier">Supplier	: <input type="text" name="supplier" id="supplier"></label>
-			</li>
-			<li>
-				<label for="jumlah">Jumlah	: <input type="text" name="jumlah" id="jumlah"></label>
-			</li>
-			<li>
-				<label for="satuan">Satuan	: <input type="text" name="satuan" id="satuan"></label>
-			</li>
-			<li>
-				<button type="submit" name="submit">Simpan</button>
-			</li>
-		</ul>
-		
+	<table>
+			<tr>
+				<td>ID Transaksi</td>
+				<td>:</td>
+				<td><input type="text" name="id_transaksi" id="id_transaksi"></td>
+			</tr>
+			<tr>
+				<td>Tanggal</td>
+				<td>:</td>
+				<td><input type="text" name="tanggal" id="tanggal"></td>
+			</tr>
+			<tr>
+				<td>Kode Barang</td>
+				<td>:</td>
+				<td><input type="text" name="kode_barang" id="kode_barang"></td>
+			</tr>
+			<tr>
+				<td>Nama Barang</td>
+				<td>:</td>
+				<td><input type="text" name="nama_barang" id="nama_barang"></td>
+			</tr>
+			<tr>
+				<td>Supplier</td>
+				<td>:</td>
+				<td><input type="text" name="supplier" id="supplier"></td>
+			</tr>
+			<tr>
+				<td>Jumlah</td>
+				<td>:</td>
+				<td><input type="text" name="jumlah" id="jumlah"></td>
+			</tr>
+			<tr>
+				<td>Satuan</td>
+				<td>:</td>
+				<td><input type="text" name="satuan" id="satuan"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td>
+					<button type="submit" name="submit">Tambah</button>
+					<button type="submit" name="batal">Batal</button>
+				</td>
+			</tr>
+		</table>
 	</form>
 
 </body>
 </html>
+
