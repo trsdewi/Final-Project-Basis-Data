540bc681086254d083e4cdd42a87e5add7d2fb45
trsdewi <76573977+trsdewi@users.noreply.github.com>
2022-06-15 11:13:18 +0800

    Update tambahcust.php

--- finalproject/tambahcust.php
+++ finalproject/tambahcust.php
@@ -30,25 +30,38 @@ if (isset($_POST["submit"])) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Data Customer</title>
-</head>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head class="form">
+	<div class="wrap">
+		<div class="container2">
 <body>
 	<h1>Input Data Customer</h1>
 	<form action="" method="post">
-		<ul>
-			<li>
-				<label for="nama_penerima">Nama Penerima	: <input type="text" name="nama_penerima" id="nama_penerima"></label>
-			</li>
-			<li>
-				<label for="alamat">Alamat	: <input type="text" name="alamat" id="alamat"></label>
-			</li>
-			<li>
-				<label for="telepon">Telepon	: <input type="text" name="telepon" id="telepon"></label>
-			</li>
-			<li>
-				<button type="submit" name="submit">Simpan</button>
-			</li>
-		</ul>
-		
+	<table>
+			<tr>
+				<td>Nama Penerima</td>
+				<td>:</td>
+				<td><input type="text" name="nama_penerima" id="nama_penerima"></td>
+			</tr>
+			<tr>
+				<td>Alamat</td>
+				<td>:</td>
+				<td><input type="text" name="alamat" id="alamat"></td>
+			</tr>
+			<tr>
+				<td>Telepon</td>
+				<td>:</td>
+				<td><input type="text" name="telepon" id="telepon"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td>
+					<button type="submit" name="submit">Tambah</button>
+					<button type="submit" name="batal">Batal</button>
+				</td>
+			</tr>
+	</table>
 	</form>
 
 </body>
